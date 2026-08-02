d6f194016fa3aec742172ad9a651f20e19f2f106
gsadmin <gsadmin@DESKTOP-R7R9J7E>
2026-08-02 19:07:38 +0700

    wip

--- fx/projectFiles/loop.h
+++ fx/projectFiles/loop.h
@@ -2186,7 +2186,7 @@ namespace Loop
 			//
 			//Object::HeroMesh.Load("..//fx//projectFiles//hero.obj");
 			//Object::MeshPtr = &Object::HeroMesh;
-			ConstBuf::LoadObj("..//fx//projectFiles//hero.glb");
+			ConstBuf::LoadObj("..//fx//projectFiles//hero_landing.glb");
 			Object::MeshPtr = nullptr;
 			float4 p = V2F(hero.pos * 10000.);
 
