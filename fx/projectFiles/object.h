57ab0aca852d3a37619b59024b8474bca082498d
gsadmin <gsadmin@DESKTOP-R7R9J7E>
2026-09-09 16:15:27 +0700

    wip

--- fx/projectFiles/object.h
+++ fx/projectFiles/object.h
@@ -1689,6 +1689,23 @@ namespace Object {
 
 #endif
 
+	/*cmd(RayHit, int xStartPos, int yStartPos, int zStartPos, int xEndPos, int yEndPos, int zEndPos)
+	{
+		reflect;
+
+		vs::Nebula2 = {
+			.params = {
+				.pos1 = float4(in.xStartPos / 10000., in.yStartPos / 10000., in.zStartPos / 10000., 0),
+				.pos2 = float4(in.xEndPos / 10000., in.yEndPos / 10000., in.zEndPos / 10000., 0)
+			},
+		};
+
+		vs::Nebula2.set();
+
+		Drawer::NullDrawer({ 1, 1 });
+
+	}*/
+
 	cmd(ScorpBall, int count, int skipper, pMode mode, int r, int g, int b)
 	{
 		reflect;
